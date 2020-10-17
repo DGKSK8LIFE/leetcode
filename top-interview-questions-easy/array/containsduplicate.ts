26dbfbf7d772b9b77586c7d6d0ff473ee001a8f5
Tarek Ali <tarekali15@outlook.com>
2020-10-17 17:33:08 -0500

    containsduplicate.ts

---
+++ top-interview-questions-easy/array/containsduplicate.ts
@@ -0,0 +1,9 @@
+// // Contains Duplicate
+// function containsDuplicate(nums: number[]): boolean {
+//   const map: object = {}
+//   for (let x of nums) {
+//     if (x in map) return true;
+//     map[x] = x;
+//   }
+//   return false;
+// };
