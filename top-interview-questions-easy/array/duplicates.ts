16cbc6fa8cc85fbff6eba5c5d52c01b9da19a3d7
Tarek Ali <tarekali15@outlook.com>
2020-10-17 15:51:09 -0500

    duplicates.ts

--- top-interview-questions-easy/array/duplicates.ts
+++ top-interview-questions-easy/array/duplicates.ts
@@ -1,1 +1,12 @@
 // Remove Duplicates from Sorted Array
+function removeDuplicates(nums: number[]): number {
+  if (nums.length == 0) return 0;
+  let i: number = 0;
+  for (let j: number = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+};
