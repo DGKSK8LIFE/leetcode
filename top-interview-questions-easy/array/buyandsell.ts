b1ad6596ad10de85eab10c58aafafe596d8092c9
Tarek Ali <tarekali15@outlook.com>
2020-10-17 16:21:09 -0500

    fixed buy and sellts

--- top-interview-questions-easy/array/buyandsell.ts
+++ top-interview-questions-easy/array/buyandsell.ts
@@ -1,28 +1,9 @@
-// // Best Time to Buy and Sell Stock II
-
-// // Brute Force
-// function maxProfitBruteForce(prices: number[]): number {
-//   let maxprofit: number = 0;
-//   for (let i: number = 0; i < prices.length - 1; i++) {
-//     for (let j: number = i + 1; j < prices.length; j++) {
-//       const profit: number = prices[j] - prices[i];
-//       if (profit > maxprofit)
-//         maxprofit = profit;
-//     }
-//   }
-//   return maxprofit;
-// }
-
-
-// // One Pass
-// function maxProfit(prices: number[]): number {
-//   let minprice: number = 0;
-//   let maxprofit: number = 0;
-//   for (let i: number = 0; i < prices.length; i++) {
-//     if (prices[i] < minprice)
-//       minprice = prices[i];
-//     else if (prices[i] - minprice > maxprofit)
-//       maxprofit = prices[i] - minprice;
-//   }
-//   return maxprofit;
-// };
+// Best Time to Buy and Sell II
+function maxProfit(prices: number[]): number {
+    let maxprofit: number = 0;
+    for (let i: number = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1])
+            maxprofit += prices[i] - prices[i - 1];
+    }
+    return maxprofit;
+}
