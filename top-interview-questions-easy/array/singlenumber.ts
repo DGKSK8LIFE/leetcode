e0fc148c78511c77400a409b258d55602c7acbf7
Tarek Ali <tarekali15@outlook.com>
2020-10-18 11:15:31 -0500

    bit manipulation completion + success in singlenumber.ts

--- top-interview-questions-easy/array/singlenumber.ts
+++ top-interview-questions-easy/array/singlenumber.ts
@@ -1,4 +1,8 @@
-// Single Number
+// Single Number (Bit Manipulation)
 function singleNumber(nums: number[]): number {
-
+  let a: number = 0;
+  for (let i of nums) {
+    a ^= i;
+  }
+  return a
 };
