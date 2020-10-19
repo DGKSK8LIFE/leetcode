f0a8c6ec923debebe006fc17a4abc686ef2fd47c
Tarek Ali <tarekali15@outlook.com>
2020-10-18 21:13:03 -0500

    plusone solution; currently returning NAN and undesired output; workin on it

--- top-interview-questions-easy/array/plusone.ts
+++ top-interview-questions-easy/array/plusone.ts
@@ -1,1 +1,25 @@
 // Plus One
+function plusOne(digits: number[]): number[] {
+  if (digits == null || digits.length == 0)
+    return new Array<number>();
+
+  let carry: number = 1;
+  let i: number = digits.length;
+  for (i - 1; i >= 0; i--) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
+    } else {
+      carry += digits[i];
+      digits[i] = carry;
+      break;
+    }
+  }
+
+  if (i < 0) {
+    const result: number[] = [digits.length + 1];
+    result[0] = 1;
+    return result;
+  } else {
+    return digits;
+  }
+};
