0f21f3d347ffa45c515999f15d781a8a15b7d6f3
Tarek Ali <tarekali15@outlook.com>
2020-10-18 20:28:54 -0500

    intersectionoftwoarrays.ts

--- top-interview-questions-easy/array/intersectiontwoarrays.ts
+++ top-interview-questions-easy/array/intersectiontwoarrays.ts
@@ -1,4 +1,27 @@
 // Intersection of Two Arrays II
 function intersect(nums1: number[], nums2: number[]): number[] {
+  nums1.sort((a, b) => a - b)
+  nums2.sort((a, b) => a - b)
+  let list: number[] = [];
+  let p1: number = 0;
+  let p2: number = 0;
+  while (p1 < nums1.length && p2 < nums2.length) {
+    if (nums1[p1] < nums2[p2]) {
+      p1++;
+    } else if (nums1[p1] > nums2[p2]) {
+      p2++;
+    } else {
+      list.push(nums1[p1]);
+      p1++;
+      p2++;
+    }
+  }
 
+  const result: number[] = [];
+  let i: number = 0;
+  while (i < list.length) {
+    result[i] = list[i];
+    i++;
+  }
+  return result;
 };
