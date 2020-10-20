35ed5289659bd91ba69625b1180738b1549ae42c
Tarek Ali <tarekali15@outlook.com>
2020-10-20 11:11:16 -0500

    improved rotate

--- top-interview-questions-easy/array/rotatearray.ts
+++ top-interview-questions-easy/array/rotatearray.ts
@@ -1,17 +1,12 @@
 // Rotate Array
-function rotate(nums: number[], k: number): void {
-  k %= nums.length;
-  reverse(nums, 0, nums.length - 1);
-  reverse(nums, 0, k - 1);
-  reverse(nums, k, nums.length - 1);
-}
-
-function reverse(nums: number[], start: number, end: number): void {
-  while (start < end) {
-    let temp: number = nums[start];
-    nums[start] = nums[end];
-    nums[end] = temp;
-    start++;
-    end--;
+function rotate(nums: number[], k: number): number[] {
+  var a: number | undefined;
+  for (var i = 0; i < k; i++) {
+    a = nums.pop()
+    if (a != undefined)
+      nums.unshift(a);
+    else
+      break;
   }
+  return nums;
 }
