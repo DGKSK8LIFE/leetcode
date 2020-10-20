a804533f9da06ffa67263ebfc83d7da9d8800c31
Tarek Ali <tarekali15@outlook.com>
2020-10-20 17:35:24 -0500

    twoSum

--- top-interview-questions-easy/array/twosum.ts
+++ top-interview-questions-easy/array/twosum.ts
@@ -1,1 +1,13 @@
 // Two Sum
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+
+  for (let i: number = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
