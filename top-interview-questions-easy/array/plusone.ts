7e51b28061c9094dd680b4e5bc7c55d7aa557031
Tarek Ali <tarekali15@outlook.com>
2020-10-20 14:07:12 -0500

    passing plusOne

--- top-interview-questions-easy/array/plusone.ts
+++ top-interview-questions-easy/array/plusone.ts
@@ -1,14 +1,14 @@
 // Plus One
 function plusOne(digits: number[]): number[] {
-  for (let i: number = digits.length - 1; i >= 0; i--) {
-    if (digits[i] < 9) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
       digits[i]++;
       return digits;
+    } else {
+      digits[i] = 0;
     }
-    digits[i] = 0;
   }
 
-  const result: number[] = [];
-  result[0] = 1;
-  return result;
+  digits.unshift(1);
+  return digits;
 };
