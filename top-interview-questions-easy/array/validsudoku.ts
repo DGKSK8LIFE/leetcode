f3168421f879543c0cf6d0c2a9e9995b6fc6bd43
Tarek Ali <tarekali15@outlook.com>
2020-10-21 20:42:23 -0500

    valid sudoku

--- top-interview-questions-easy/array/validsudoku.ts
+++ top-interview-questions-easy/array/validsudoku.ts
@@ -1,1 +1,29 @@
 // Valid Sudoku
+function isValidSudoku(board: string[][]): boolean {
+  for (let i: number = 0; i < 9; i++) {
+    const row = new Set(),
+      col = new Set(),
+      box = new Set();
+
+    for (let j: number = 0; j < 9; j++) {
+      const _row: string = board[i][j];
+      const _col: string = board[j][i];
+      const _box: string = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
+
+      if (_row != '.') {
+        if (row.has(_row)) return false;
+        row.add(_row);
+      }
+      if (_col != '.') {
+        if (col.has(_col)) return false;
+        col.add(_col);
+      }
+
+      if (_box != '.') {
+        if (box.has(_box)) return false;
+        box.add(_box);
+      }
+    }
+  }
+  return true;
+};
