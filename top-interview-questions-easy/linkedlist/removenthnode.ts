35a65b07dfac9a8adcf88a11512956b1d9dfd8d5
Tarek Ali <tarekali15@outlook.com>
2020-10-27 08:02:51 -0500

    removenthnode.ts

--- top-interview-questions-easy/linkedlist/removenthnode.ts
+++ top-interview-questions-easy/linkedlist/removenthnode.ts
@@ -1,4 +1,13 @@
 // Remove Nth Node From End of List
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
   let hare: ListNode = head, curr: ListNode = head;
   while (n--) {
