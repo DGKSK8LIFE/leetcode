c018e15f90773aed73d2835c2f68af799379f44f
Tarek Ali <tarekali15@outlook.com>
2020-10-31 09:55:14 -0500

    2 files

---
+++ top-interview-questions-easy/design/minstack.ts
@@ -0,0 +1,31 @@
+// Min Stack
+class MinStack {
+  constructor() {
+
+  }
+
+  push(x: number): void {
+
+  }
+
+  pop(): void {
+
+  }
+
+  top(): number {
+
+  }
+
+  getMin(): number {
+
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
---
+++ top-interview-questions-easy/design/shuffleanarray.ts
@@ -0,0 +1,21 @@
+// Shuffle an Array
+class Solution {
+  constructor(nums: number[]) {
+
+  }
+
+  reset(): number[] {
+
+  }
+
+  shuffle(): number[] {
+
+  }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
