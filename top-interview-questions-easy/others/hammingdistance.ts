33e1ba4f6c454886a8966b530f1f51425189406c
Tarek Ali <tarekali15@outlook.com>
2020-11-03 12:51:12 -0600

    hammingdistance

--- top-interview-questions-easy/others/hammingdistance.ts
+++ top-interview-questions-easy/others/hammingdistance.ts
@@ -1,4 +1,14 @@
 // Hamming Distance
 function hammingDistance(x: number, y: number): number {
+  let value: number = x ^ y;
+  let counter: number = 0;
 
+  while (value != 0) {
+    if (value & 1)
+      ++counter
+
+    value = value >> 1;
+  }
+
+  return counter;
 };
