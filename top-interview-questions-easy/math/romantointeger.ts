4e23a41d36d2e4264fa3b928f31ae0ef80810d06
Tarek Ali <tarekali15@outlook.com>
2020-11-04 20:34:59 -0600

    romantointeger.ts (couldnt get this working; oh well)

--- top-interview-questions-easy/math/romantointeger.ts
+++ top-interview-questions-easy/math/romantointeger.ts
@@ -1,39 +1,18 @@
 // Roman to Integer
+const symbols = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+};
 
-/**
- * @param {string} s
- * @return {number}
- */
 function romanToInt(s: string): number {
-  const symbols = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+  let value: number = 0;
+  for (let i: number = 0; i < s.length; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]] ? value -= symbols[s[i]] : value += symbols[s[i]];
   }
-
-  let decimal: number = 0;
-  for (let i: number = 0; i < s.length; i++) {
-    if (s[i] === 'I' && s[i + 1] && (s[i + 1] === 'V' || s[i + 1] === 'X')) {
-      decimal -= 1;
-      continue;
-    }
-
-    if (s[i] === 'X' && s[i + 1] && (s[i + 1] === 'L' || s[i + 1] === 'C')) {
-      decimal -= 10;
-      continue;
-    }
-
-    if (s[i] === 'C' && s[i + 1] && (s[i + 1] === 'D' || s[i + 1] === 'M')) {
-      decimal -= 100;
-      continue;
-    }
-
-    decimal += symbols[s[i]];
-  }
-
-  return decimal;
+  return value;
 };
